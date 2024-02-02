6aaa180a2cf08e74e174f5b2e06a85277a9aaf10
agolzar1 <156872398+agolzar1@users.noreply.github.com>
2024-02-02 18:01:19 +0000

    Lab 4 submitted

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function setup() {
   createCanvas(320, 240);
 }
+var a = 30 , v = 30;
+var diameter = 50;
 
 function draw() {
   background(200);
@@ -8,11 +10,17 @@ function draw() {
   // check to condition of the mouse
   if(mouseIsPressed){ // true or false?
     // if true, change the fill color here
-    
+    fill (0,365,0);
+    a = width/5
+    v = height/5
+    diameter = 100;
   }
   else { 
     // otherwise do this:
-    
+    fill(365);
+    a = width/3;
+    v = height/3;
+   diameter = 50;
   }
-  ellipse(width/2, height/2, 100, 100);
+  ellipse(a, v, diameter);
 }
